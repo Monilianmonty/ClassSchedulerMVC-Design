aa36e561216008ca507a6713c25969c425a3bcf1
jjs0805 <118850449+jjs0805@users.noreply.github.com>
2022-11-23 17:40:31 -0500

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Class implements Serializable{
+	String name;
+	String title;
+	String start;
+	String end;
+	
+	public Class(String n, String t, String s, String e) {
+		name = n;
+		title = t;
+		start = s;
+		end = e;
+	}
+	
+	
+}
