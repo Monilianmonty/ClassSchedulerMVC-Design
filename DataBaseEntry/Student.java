c40b7a82287c66eda96b509d0b075513ffda8ca1
Mathias Nebiyou <93014669+Monilianmonty@users.noreply.github.com>
2022-11-29 22:14:21 -0500

    Update Student.java

--- DataBaseEntry/Student.java
+++ DataBaseEntry/Student.java
@@ -1,30 +1,38 @@
+
+
 import java.io.*;
 
 public class Student implements Serializable{
-	int id;
-	String name;
-	String email;
-	String password;
-	
-	public Student(int i, String n, String e, String p) {
-		id = i;
-		name = n;
-		email = e;
-		password = p;
-	}
+    String id;
+    String name;
+    String email;
+    String password;
 
-	public int getId() {return id;}
-	public void setId(int id) {this.id = id;}
+    public Student(String i, String n, String e, String p) {
+        id = i;
+        name = n;
+        email = e;
+        password = p;
+    }
 
-	public String getName() {return name;}
-	public void setName(String name) {this.name = name;}
-	
-	public String getEmail() {return email;}
-	public void setEmail(String email) {this.email = email;}
+    public Student(){
 
-	public String getPassword() {return password;}
-	public void setPassword(String password) {this.password = password;}
-	
-	
-	
+    }
+
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public void Display(){
+        System.out.println(getId() + " " + getEmail() + " " + getPassword() +" "+ getName());
+    }
+
 }
