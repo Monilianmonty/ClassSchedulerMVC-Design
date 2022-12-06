c3ff5fdd2f5fba74078e21e29bacb4b54526221d
Mathias Nebiyou <93014669+Monilianmonty@users.noreply.github.com>
2022-12-05 21:27:50 -0500

    Update UserRegistration.java

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -72,27 +72,27 @@ public class UserRegistration extends JFrame implements ActionListener,Serializa
         email.setSize(250,40);
         email.setLocation(100,160);
         p1.add(email);
-        
+
         //email textbox
         temail = new JTextField();
         temail.setFont(new Font("Times New Roman", Font.PLAIN, 15));
         temail.setSize(190, 25);
         temail.setLocation(155,170);
         p1.add(temail);
-        
+
         //password label
         upassword = new JLabel("Password");
         upassword.setFont(new Font("Times New Roman", Font.BOLD, 20));
         upassword.setSize(250,40);
         upassword.setLocation(70,200);
         p1.add(upassword);
-        
+
         //password textfield
         tpassword = new JPasswordField();
         tpassword.setLocation(155,208);
         tpassword.setSize(190,25);
         p1.add(tpassword);
-        
+
         //submit button
         submit = new JButton("Submit");
         submit.setBounds(160, 250, 70, 25);
@@ -101,7 +101,7 @@ public class UserRegistration extends JFrame implements ActionListener,Serializa
         submit.setFont(new Font("Times New Roman", Font.BOLD,10));
         submit.addActionListener(new UserRegistration());
         p1.add(submit);
-        
+
         //clear all text fields
         reset = new JButton("Reset");
         reset.setBounds(240, 250, 70, 25);
@@ -110,16 +110,16 @@ public class UserRegistration extends JFrame implements ActionListener,Serializa
         reset.setFont(new Font("Times New Roman", Font.BOLD,10));
         reset.addActionListener(new UserRegistration());
         p1.add(reset);
-        
+
         //go back to login page
         login = new JButton("Log In page");
-        login.setBounds(200, 300, 100, 25);
+        login.setBounds(180, 290, 100, 25);
         login.setForeground(Color.WHITE);
         login.setBackground(Color.BLACK);
         login.setFont(new Font("Times New Roman", Font.BOLD, 10));
         login.addActionListener(new UserRegistration());
         p1.add(login);
-        
+
         //make the frame visible
         regFrame.setVisible(true);
 
@@ -139,7 +139,7 @@ public class UserRegistration extends JFrame implements ActionListener,Serializa
         String newEmail = temail.getText();
         String newPassword = String.valueOf(tpassword.getPassword());
         boolean takenStudent = false;
-        
+
         //go back to login page
         if(e.getSource() == login){
             regFrame.setVisible(false);
@@ -186,51 +186,51 @@ public class UserRegistration extends JFrame implements ActionListener,Serializa
 
             readf.useDelimiter("\\n|,");
 
-            //read the objects available in the database 
+            //read the objects available in the database
 
-                if(emailChar) {
-                    while (userf.available() > 0) {
-                        Student sf = (Student) of.readObject();
-                        String Uem = sf.getEmail();
-                        String UID = sf.getId();
-                        //if the user has entered an email,id, or password that is already taken then error message occurs
-                        if (e.getSource() == submit) {
-                            if (newEmail.equals("")) {
-                                JOptionPane.showMessageDialog(null, "Please enter email");
+            if(emailChar) {
+                while (userf.available() > 0) {
+                    Student sf = (Student) of.readObject();
+                    String Uem = sf.getEmail();
+                    String UID = sf.getId();
+                    //if the user has entered an email,id, or password that is already taken then error message occurs
+                    if (e.getSource() == submit) {
+                        if (newEmail.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please enter email");
 
-                            } else if (newName.equals("")) {
-                                JOptionPane.showMessageDialog(null, "Please enter name");
+                        } else if (newName.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please enter name");
 
-                            } else if (newID.equals("")) {
-                                JOptionPane.showMessageDialog(null, "Please enter ID");
+                        } else if (newID.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please enter ID");
 
-                            } else if (newPassword.equals("")) {
-                                JOptionPane.showMessageDialog(null, "Please enter password");
+                        } else if (newPassword.equals("")) {
+                            JOptionPane.showMessageDialog(null, "Please enter password");
 
-                            } else if (newEmail.equals(Uem)) {
-                                JOptionPane.showMessageDialog(null, "Email already taken");
+                        } else if (newEmail.equals(Uem)) {
+                            JOptionPane.showMessageDialog(null, "Email already taken");
 
-                            } else if (newID.equals(UID)) {
-                                JOptionPane.showMessageDialog(null, "ID already taken");
+                        } else if (newID.equals(UID)) {
+                            JOptionPane.showMessageDialog(null, "ID already taken");
 
-                            }else {
-                                JOptionPane.showMessageDialog(null, "Registration Complete!");
-                                takenStudent = true;
-                                regFrame.setVisible(false);
-                                LogInPageInterface.drawLog();
+                        }else {
+                            JOptionPane.showMessageDialog(null, "Registration Complete!");
+                            takenStudent = true;
+                            regFrame.setVisible(false);
+                            LogInPageInterface.drawLog();
 
-                            }
                         }
                     }
                 }
-
-            } catch(FileNotFoundException ex){
-                throw new RuntimeException(ex);
-            } catch(IOException ex){
-                throw new RuntimeException(ex);
-            } catch(ClassNotFoundException ex){
-                throw new RuntimeException(ex);
             }
+
+        } catch(FileNotFoundException ex){
+            throw new RuntimeException(ex);
+        } catch(IOException ex){
+            throw new RuntimeException(ex);
+        } catch(ClassNotFoundException ex){
+            throw new RuntimeException(ex);
+        }
 
 
 
