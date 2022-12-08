0cc341c87cd1b57af8e49603d8781c67761a5cf6
jjs0805 <118850449+jjs0805@users.noreply.github.com>
2022-12-08 01:24:56 -0500

    Update Login_View.java

--- New_Code/Login_View.java
+++ New_Code/Login_View.java
@@ -1,117 +1,217 @@
 package final_project_MVC;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Login_View extends JPanel{
-	private JFrame loginFrame;
-	private JLabel login_userLabel;
-	private JLabel login_passLabel;
-	private JTextField login_userTextField;
-	private JPasswordField login_passTextField;
-	private JButton loginButton;
-	private JButton registerButton;
+public class UserReg_View extends JPanel{
+	protected JFrame reg_Frame;
+	private JLabel reg_nameLabel;
+	private JTextField reg_nameTextField;
+	private JLabel reg_idLabel;
+	private JTextField reg_idTextField;
+	private JLabel reg_userLabel;
+	private JTextField reg_userTextField;
+	private JLabel reg_passLabel;
+	private JTextField reg_passTextField;
+	private JButton reg_submit;
+	private JButton reg_reset;
+	private JButton back_to_login;
 	
-	public Login_View() {
-			
-			this.setLayout(null);
-			this.setVisible(true);
-			
-			loginFrame = new JFrame("Log In: ");
-			loginFrame.setLocation(new Point(300, 300));
-			loginFrame.setSize(new Dimension(400, 200));
-			loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			loginFrame.add(this);
-			
-			login_userLabel = new JLabel("Email:");
-			login_userLabel.setBounds(100, 8, 70, 20);
-			this.add(login_userLabel);
+	public UserReg_View() {
 		
-			login_userTextField = new JTextField();
-			login_userTextField.setBounds(100, 27, 193, 28);
-			this.add(login_userTextField);
-			
-			login_passLabel = new JLabel("Password:");
-			login_passLabel.setBounds(100, 55, 70, 20);
-			this.add(login_passLabel);
-			
-			login_passTextField = new JPasswordField();
-			login_passTextField.setBounds(100, 75, 193, 28);
-			this.add(login_passTextField);
-			
-			loginButton = new JButton("Log In");
-			loginButton.setBounds(100,110,70,25);
-	        loginButton.setForeground(Color.WHITE);
-	        loginButton.setBackground(Color.BLACK);
-	        loginButton.setFont(new Font("Arial", Font.BOLD, 10));
-	        this.add(loginButton);
-	        
-	        registerButton = new JButton("Register");
-	        registerButton.setBounds(200, 110, 90, 25);
-	        registerButton.setForeground(Color.WHITE);
-	        registerButton.setBackground(Color.BLACK);
-	        registerButton.setFont(new Font("Arial", Font.BOLD, 10));
-	        this.add(registerButton);
-	        
-	        loginFrame.setVisible(true);
+        this.setLayout(null);
+        
+        //declare fonts for ease of use
+        Font bold = new Font("Times New Roman", Font.BOLD, 20);
+        Font plain = new Font("Times New Roman", Font.PLAIN, 15);
+        
+        
+        //set up window
+        reg_Frame = new JFrame("User Registration");
+        reg_Frame.setLocation(new Point(700, 300));
+        reg_Frame.add(this);
+        reg_Frame.setSize(new Dimension(500, 500));
+        reg_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        //Student Name Label and entry box
+        reg_nameLabel = new JLabel("Name: ");
+        reg_nameLabel.setFont(bold);
+        reg_nameLabel.setSize(250,40);
+        reg_nameLabel.setLocation(100, 100);
+        this.add(reg_nameLabel);
+        reg_nameTextField = new JTextField();
+        reg_nameTextField.setFont(plain);
+        reg_nameTextField.setSize(190, 25);
+        reg_nameTextField.setLocation(155, 110);
+        this.add(reg_nameTextField);
+        
+        
+        //Student ID Label and entry box
+        reg_idLabel = new JLabel(" ID: ");
+        reg_idLabel.setFont(bold);
+        reg_idLabel.setSize(250, 40);
+        reg_idLabel.setLocation(120, 130);
+        this.add(reg_idLabel);
+        reg_idTextField = new JTextField();
+        reg_idTextField.setFont(plain);
+        reg_idTextField.setSize(190, 25);
+        reg_idTextField.setLocation(155, 140);
+        this.add(reg_idTextField);
+        
+        
+        //Student email Label and entry box
+        reg_userLabel = new JLabel("Email: ");
+        reg_userLabel.setFont(bold);
+        reg_userLabel.setSize(250, 40);
+        reg_userLabel.setLocation(100, 160);
+        this.add(reg_userLabel);
+        reg_userTextField = new JTextField();
+        reg_userTextField.setFont(plain);
+        reg_userTextField.setSize(190, 25);
+        reg_userTextField.setLocation(155, 170);
+        this.add(reg_userTextField);
+        
+        
+        //Student Password Label and entry box
+        reg_passLabel = new JLabel("Password: ");
+        reg_passLabel.setFont(bold);
+        reg_passLabel.setSize(250, 40);
+        reg_passLabel.setLocation(70, 190);
+        this.add(reg_passLabel);
+        reg_passTextField = new JTextField();
+        reg_passTextField.setFont(plain);
+        reg_passTextField.setSize(190, 25);
+        reg_passTextField.setLocation(155, 200);
+        this.add(reg_passTextField);
+        
+        
+        //Submit button Setup (functionality in controller)
+        reg_submit = new JButton("Submit");
+        reg_submit.setBounds(160, 250, 70, 25);
+        reg_submit.setForeground(Color.WHITE);
+        reg_submit.setBackground(Color.BLACK);
+        reg_submit.setFont(new Font("Times New Roman", Font.BOLD,10));
+        this.add(reg_submit);
+        
+        //Reset Button Setup
+        reg_reset = new JButton("Reset");
+        reg_reset.setBounds(240, 250, 70, 25);
+        reg_reset.setForeground(Color.WHITE);
+        reg_reset.setBackground(Color.BLACK);
+        reg_reset.setFont(new Font("Times New Roman", Font.BOLD,10));
+        this.add(reg_reset);
+        
+        //Back to Login
+        back_to_login = new JButton("Back");
+        back_to_login.setBounds(180, 290, 100, 25);
+        back_to_login.setForeground(Color.WHITE);
+        back_to_login.setBackground(Color.BLACK);
+        back_to_login.setFont(new Font("Times New Roman", Font.BOLD, 10));
+        this.add(back_to_login);
 	}
 
-	public JFrame getLoginFrame() {
-		return loginFrame;
+	public JFrame getReg_Frame() {
+		return reg_Frame;
 	}
 
-	public void setLoginFrame(JFrame loginFrame) {
-		this.loginFrame = loginFrame;
+	public void setReg_Frame(JFrame reg_Frame) {
+		this.reg_Frame = reg_Frame;
 	}
 
-	public JLabel getUsernameLabel() {
-		return login_userLabel;
+	public JLabel getReg_nameLabel() {
+		return reg_nameLabel;
 	}
 
-	public void setUsernameLabel(JLabel login_userLabel) {
-		this.login_userLabel = login_userLabel;
+	public void setReg_nameLabel(JLabel reg_nameLabel) {
+		this.reg_nameLabel = reg_nameLabel;
 	}
 
-	public JLabel getPassLabel() {
-		return login_passLabel;
+	public JTextField getReg_nameTextField() {
+		return reg_nameTextField;
 	}
 
-	public void setPassLabel(JLabel login_passLabel) {
-		this.login_passLabel = login_passLabel;
+	public void setReg_nameTextField(JTextField reg_nameTextField) {
+		this.reg_nameTextField = reg_nameTextField;
 	}
 
-	public JTextField getUsernameTextField() {
-		return login_userTextField;
+	public JLabel getReg_idLabel() {
+		return reg_idLabel;
 	}
 
-	public void setUsernameTextField(JTextField login_userTextField) {
-		this.login_userTextField = login_userTextField;
+	public void setReg_idLabel(JLabel reg_idLabel) {
+		this.reg_idLabel = reg_idLabel;
 	}
 
-	public JPasswordField getPassTextField() {
-		return login_passTextField;
+	public JTextField getReg_idTextField() {
+		return reg_idTextField;
 	}
 
-	public void setPassTextField(JPasswordField login_passTextField) {
-		this.login_passTextField = login_passTextField;
+	public void setReg_idTextField(JTextField reg_idTextField) {
+		this.reg_idTextField = reg_idTextField;
 	}
 
-	public JButton getLoginButton() {
-		return loginButton;
+	public JLabel getReg_userLabel() {
+		return reg_userLabel;
 	}
 
-	public void setLoginButton(JButton loginButton) {
-		this.loginButton = loginButton;
+	public void setReg_userLabel(JLabel reg_userLabel) {
+		this.reg_userLabel = reg_userLabel;
 	}
 
-	public JButton getRegisterButton() {
-		return registerButton;
+	public JTextField getReg_userTextField() {
+		return reg_userTextField;
 	}
 
-	public void setRegisterButton(JButton registerButton) {
-		this.registerButton = registerButton;
+	public void setReg_userTextField(JTextField reg_userTextField) {
+		this.reg_userTextField = reg_userTextField;
 	}
-	
-	
+
+	public JLabel getReg_passLabel() {
+		return reg_passLabel;
+	}
+
+	public void setReg_passLabel(JLabel reg_passLabel) {
+		this.reg_passLabel = reg_passLabel;
+	}
+
+	public JTextField getReg_passTextField() {
+		return reg_passTextField;
+	}
+
+	public void setReg_passTextField(JTextField reg_passTextField) {
+		this.reg_passTextField = reg_passTextField;
+	}
+
+	public JButton getReg_submit() {
+		return reg_submit;
+	}
+
+	public void setReg_submit(JButton reg_submit) {
+		this.reg_submit = reg_submit;
+	}
+
+	public JButton getReg_reset() {
+		return reg_reset;
+	}
+
+	public void setReg_reset(JButton reg_reset) {
+		this.reg_reset = reg_reset;
+	}
+
+	public JButton getBack_to_login() {
+		return back_to_login;
+	}
+
+	public void setBack_to_login(JButton back_to_login) {
+		this.back_to_login = back_to_login;
+	}
 }
