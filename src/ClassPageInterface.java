42908b63fbe2eb46ebcfd17d4a99a25acc1d7c50
Carter Thompson <cartert54@users.noreply.github.com>
2022-12-07 23:02:54 -0500

    Update ClassPageInterface.java

--- src/ClassPageInterface.java
+++ src/ClassPageInterface.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Array;
 
 public class ClassPageInterface extends JFrame{
     //serves as an incrementer for the position of a course
@@ -38,14 +37,13 @@ public class ClassPageInterface extends JFrame{
         layout.putConstraint(SpringLayout.WEST, addClass, 5, SpringLayout.WEST, contentPane);
 
         //temporary classes (courses) for now
-        boolean[] MW = {true, false, true, false, false};
-        boolean[] MWF = {true, false, true, false, true};
-
-
-        Class class1 = new Class("SWENG311", "Object Oriented Programming", "10:00", "11:00", MW);
-        Class class2 = new Class("CMPSC360", "Discrete Mathematics", "11:00", "12:00", MWF);
-        Class class3 = new Class("CMPSC121", "Intermediate Programming", "12:00", "13:00", MWF);
-        Class class4 = new Class("CMPSC200", "Matlab", "12:00", "13:00", MWF);
+        boolean[] MWF = new boolean[]{true, false, true, false, true};
+        boolean[] MWTF = new boolean[]{true, false, true, true, false};
+        boolean[] TR = new boolean[]{false, true, false, true, false};
+        Class class1 = new Class("SWENG311", "Object Oriented Programming", 10, 11, MWTF);
+        Class class2 = new Class("CMPSC360", "Discrete Mathematics", 11, 12, MWF);
+        Class class3 = new Class("CMPSC121", "Intermediate Programming", 12, 13, MWTF);
+        Class class4 = new Class("CMPSC200", "Matlab", 12, 13, MWF);
         Class[] classes = new Class[]{class1,class2,class3,class4};
         //JComboBox for adding classes
         JComboBox addClassOptions = new JComboBox(classes);
@@ -56,7 +54,7 @@ public class ClassPageInterface extends JFrame{
         //JButton to add classes
         JButton addButton = new JButton("Add Class");
         contentPane.add(addButton);
-        layout.putConstraint(SpringLayout.WEST, addButton, 110, SpringLayout.WEST, addClassOptions);
+        layout.putConstraint(SpringLayout.WEST, addButton, 200, SpringLayout.WEST, addClassOptions);
         layout.putConstraint(SpringLayout.NORTH, addButton, 20, SpringLayout.NORTH, addClass);
 
         //selected classes label
@@ -66,26 +64,21 @@ public class ClassPageInterface extends JFrame{
         layout.putConstraint(SpringLayout.NORTH, selectedClasses, 25, SpringLayout.SOUTH, addClassOptions);
 
         //makes table for selected classes
-        String[] columnNames = {"Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        Object[][] data = new Object[14][6];
-        for(int i = 0; i < 4; i++) {
-            data[0][i] = columnNames[i];
+        String[] columnNames = {"Time Slot", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        Object[][] data = new Object[28][6];
+        for (int i = 0; i < 27; i+=2) {
+            data[i+1][0] = (i+8-(i/2))+":00 - "+(i+9-(i/2))+":00";
+            if (i < 26) {
+                data[i+2][0] = " ";
+            }
         }
-        for (int i = 0; i < 13; i++) {
-            data[i+1][0] = (i+8)+":00";
+        for (int i = 0; i < 6; i++) {
+            data[0][i] = columnNames[i];
         }
         JTable selectedClassesTable = new JTable(data, columnNames);
         contentPane.add(selectedClassesTable);
-        layout.putConstraint(SpringLayout.WEST, selectedClassesTable, 5, SpringLayout.WEST, contentPane);
         layout.putConstraint(SpringLayout.NORTH, selectedClassesTable, 20, SpringLayout.NORTH, selectedClasses);
-        selectedClassesTable.setCellSelectionEnabled(false);
-        DefaultTableModel tableModel = new DefaultTableModel(data, columnNames){
-            @Override
-            public boolean isCellEditable(int row, int column){
-                return false;
-            }
-        };
-        selectedClassesTable.setModel(tableModel);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, selectedClassesTable, 0, SpringLayout.HORIZONTAL_CENTER, contentPane);
 
         //JLabel for remove classes section
         JLabel removeClass = new JLabel("Select classes to remove: ");
@@ -102,7 +95,7 @@ public class ClassPageInterface extends JFrame{
         //JButton for remove classes section
         JButton removeButton = new JButton("Remove Class");
         contentPane.add(removeButton);
-        layout.putConstraint(SpringLayout.WEST, removeButton, 5, SpringLayout.EAST, removeClassOptions);
+        layout.putConstraint(SpringLayout.WEST, removeButton, 200, SpringLayout.WEST, removeClassOptions);
         layout.putConstraint(SpringLayout.NORTH, removeButton, 20, SpringLayout.NORTH, removeClass);
 
         //allows "Add Class" button to do stuff
@@ -113,15 +106,18 @@ public class ClassPageInterface extends JFrame{
                 }
                 else {
                     Class pickedClass = (Class) addClassOptions.getSelectedItem();
-                    for (int i = 0; i < 13; i++) {
-                        if (data[i][0].equals(pickedClass.getStart()) && data[i][1] == null) {
-                            selectedClassesTable.setValueAt(pickedClass.getEnd(), i, 1);
-                            selectedClassesTable.setValueAt(pickedClass.getName(), i, 2);
-                            selectedClassesTable.setValueAt(pickedClass.getTitle(), i, 3);
+                    for (int i = 1; i < 27; i++) {
+                        if (data[i][0].equals(pickedClass.intToTimeSlot(pickedClass.getStart(), pickedClass.getEnd())) && data[i][1] == null) {
+                            for (int j = 0; j < 5; j++) {
+                                if (pickedClass.getDays()[j]) {
+                                    selectedClassesTable.setValueAt(pickedClass.getName(), i, j+1);
+                                    selectedClassesTable.setValueAt(pickedClass.getTitle(), i+1, j+1);
+                                }
+                            }
                             removeClassOptions.addItem(pickedClass);
                             addClassOptions.removeItemAt(addClassOptions.getSelectedIndex());
                         }
-                        else if (data[i][1] != null && data[i][0].equals(pickedClass.getStart())){
+                        else if (data[i][1] != null && data[i][0].equals(pickedClass.intToTimeSlot(pickedClass.getStart(), pickedClass.getEnd()))){
                             cpi.dispJOP("That time slot is reserved for another class. Remove the class in this slot to add your current selected one.");
                         }
                     }
@@ -129,40 +125,37 @@ public class ClassPageInterface extends JFrame{
                 cpi.incrementi();
             }
         });
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+        renderer.setHorizontalAlignment( JLabel.CENTER );
+        selectedClassesTable.getColumnModel().getColumn(0).setCellRenderer( renderer );
+        for (int i = 1; i < 6; i++) {
+            selectedClassesTable.getColumnModel().getColumn(i).setPreferredWidth(200);
+            selectedClassesTable.getColumnModel().getColumn(i).setCellRenderer( renderer );
+        }
         //allows "Remove Class" button to do stuff
         removeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //adds course back to the "Add Class" dropdown
                 Class pickedClass = (Class) removeClassOptions.getSelectedItem();
-                for (int i = 0; i < 13; i++) {
-                    if (data[i][0].equals(pickedClass.getStart())) {
-                        selectedClassesTable.setValueAt(null, i, 1);
-                        selectedClassesTable.setValueAt(null, i, 2);
-                        selectedClassesTable.setValueAt(null,i,3);
-                        addClassOptions.addItem(removeClassOptions.getSelectedItem());
-                        removeClassOptions.removeItemAt(removeClassOptions.getSelectedIndex());
+                for (int i = 1; i < 27; i++) {
+                    if (data[i][0].equals(pickedClass.intToTimeSlot(pickedClass.getStart(), pickedClass.getEnd())) && data[i][1] != null) {
+                        for (int j = 0; j < 5; j++) {
+                            if(pickedClass.getDays()[j]) {
+                                selectedClassesTable.setValueAt(null, i, j+1);
+                                selectedClassesTable.setValueAt(null, i+1, j+1);
+                            }
+                        }
                     }
                 }
+                addClassOptions.addItem(removeClassOptions.getSelectedItem());
+                removeClassOptions.removeItemAt(removeClassOptions.getSelectedIndex());
             }
         });
-
-        //log out button
-        JButton logout = new JButton("Log Out");
-        contentPane.add(logout);
-        layout.putConstraint(SpringLayout.SOUTH, logout,-5, SpringLayout.SOUTH, contentPane);
-        layout.putConstraint(SpringLayout.EAST, logout, -5, SpringLayout.EAST, contentPane);
-        logout.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                //window.dispose();
-                window.dispose();
-                LogInPageInterface.drawLog();
-
-            }
-        });
-
         window.setVisible(true);
         window.setBounds(400,150,1280,720);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(dim.width/2-window.getSize().width/2, dim.height/2-window.getSize().height/2);
     }
 
 
